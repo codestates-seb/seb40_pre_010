38e753e31fd8cc8b748650499257e4d59b76434d
jaehongg <tkwh1133@gamil.com>
2022-10-27 14:25:27 +0900

    🔨 refactor :  컴포넌트 1차조립
    Route 설정하기, pages 폴더생성

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/footer';
+import NavSide1 from './components/Nav-Side1';
+import NavSide2 from './components/Nav-Side2';
+import TopMainNav from './components/TopMainNav';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <>
+      <Router>
+        <TopMainNav />
+        <div className="d-flex">
+          <NavSide1 />
+          <main></main>
+          <NavSide2 />
+        </div>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
