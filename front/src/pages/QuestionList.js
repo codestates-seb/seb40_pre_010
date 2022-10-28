5d1522fba88755219eb9900747b580993074ed7a
choizia0724 <drama_days@naver.com>
2022-10-28 16:03:07 +0900

    ⛲ feat : 상세페이지 추가 및 리스트  레이아웃 변경

--- front/src/pages/QuestionList.js
+++ front/src/pages/QuestionList.js
@@ -1,4 +1,6 @@
 import dummy1 from '../static/dummy1.json';
+import NavSide1 from './../components/Nav-Side1';
+import NavSide2 from './../components/Nav-Side2';
 import React from 'react';
 import styled from 'styled-components';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -6,10 +8,25 @@ const BREAK_POINT_TABLET = 768;
 const BREAK_POINT_PC = 1200;
 
 const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
   width: 100%;
   margin-right: 1em;
   > div:first-child {
     border-width: 0 !important;
+  }
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+  > div:last-child {
+    padding-top: 65px;
+  }
+`;
+const PostWrapper = styled.div`
+  width: 100%;
+  padding-right: 15px;
+  @media screen and (min-width: 376px) and (max-width: 768px) {
+    padding-right: 0;
   }
 `;
 const TagWrap = styled.div`
@@ -18,26 +35,43 @@ const TagWrap = styled.div`
 const QuestionList = () => {
   return (
     <Wrapper>
-      {dummy1.Question.map((x, i) => {
-        return (
-          <div className="ta-left bt bc-black-100 p12 md:pl12 pl64" key={i}>
-            <Link className="s-link fs-body3">{x.question_title}</Link>
-            <p className="ta-left py12">{x.question_body}</p>
-            <div className="d-flex jc-space-between">
-              <TagWrap className="mb12">
-                {x.question_tags.split(',').map((z, j) => {
-                  return (
-                    <a key={j} href="/" className="s-tag mr4">
-                      {z}
-                    </a>
-                  );
-                })}
-              </TagWrap>
-              <div>{x.question_author}</div>
-            </div>
-          </div>
-        );
-      })}
+      <NavSide1 />
+      <div className="pt96">
+        <div className=" ta-left px16 d-flex jc-space-between pb8">
+          <span className="fs-headline1">All Questions</span>
+          <button className="s-btn s-btn__primary">Ask Question</button>
+        </div>
+        <div className="d-flex">
+          <PostWrapper>
+            {dummy1.Question.map((x, i) => {
+              return (
+                <div
+                  className="ta-left bt bc-black-100 p12 md:pl12 pl64"
+                  key={i}
+                >
+                  <Link to={'/post/' + i} className="s-link fs-body3">
+                    {x.question_title}
+                  </Link>
+                  <p className="ta-left py12">{x.question_body}</p>
+                  <div className="d-flex jc-space-between">
+                    <TagWrap className="mb12">
+                      {x.question_tags.split(',').map((z, j) => {
+                        return (
+                          <a key={j} href="/" className="s-tag mr4">
+                            {z}
+                          </a>
+                        );
+                      })}
+                    </TagWrap>
+                    <div>{x.question_author}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </PostWrapper>
+        </div>
+      </div>
+      <NavSide2 className="side2" />
     </Wrapper>
   );
 };
--- front/src/pages/QuestionPost.js
+++ front/src/pages/QuestionPost.js
@@ -1,4 +1,5 @@
 import dummy1 from '../static/dummy1.json';
+import NavSide1 from './../components/Nav-Side1';
 import NavSide2 from './../components/Nav-Side2';
 import styled from 'styled-components';
 import { React, useRef } from 'react';
@@ -8,7 +9,6 @@ import '@toast-ui/editor/dist/toastui-editor.css';
 
 const Wrapper = styled.div`
   width: 100%;
-  padding: 24px 16px;
   .fs-headline1 {
     text-align: left;
     margin-bottom: 8px;
@@ -45,73 +45,76 @@ const QuestionPost = () => {
   };
 
   return (
-    <Wrapper>
-      <div className="fs-headline1">{Post.question_title}</div>
-      <div className="d-flex pb8 mb16 bb bc-black-075">
-        <p className="fc-black-500 mr24">
-          Asked <span className="fc-black-800">{Asked}</span>
-        </p>
-        <p className="fc-black-500 mr24">
-          Modified <span className="fc-black-800">{Modified}</span>
-        </p>
-        <p className="fc-black-500">
-          Viewed <span className="fc-black-800">{Viewed}</span>
-        </p>
-      </div>
-      <MainBox className="d-flex jc-space-between md:fd-column">
-        <PostArea>
-          <div className="bb bc-black-075">
-            <p
-              dangerouslySetInnerHTML={{ __html: PostBody }}
-              className="mb24"
-            ></p>
-            <p>
-              {Post.question_tags.split(',').map((z, j) => {
+    <Wrapper className="d-flex">
+      <NavSide1 />
+      <div className="pt96 pb24 px16">
+        <div className="fs-headline1">{Post.question_title}</div>
+        <div className="d-flex pb8 mb16 bb bc-black-075">
+          <p className="fc-black-500 mr24">
+            Asked <span className="fc-black-800">{Asked}</span>
+          </p>
+          <p className="fc-black-500 mr24">
+            Modified <span className="fc-black-800">{Modified}</span>
+          </p>
+          <p className="fc-black-500">
+            Viewed <span className="fc-black-800">{Viewed}</span>
+          </p>
+        </div>
+        <MainBox className="d-flex jc-space-between md:fd-column">
+          <PostArea className="pr16">
+            <div className="bb bc-black-075">
+              <p
+                dangerouslySetInnerHTML={{ __html: PostBody }}
+                className="mb24"
+              ></p>
+              <p>
+                {Post.question_tags.split(',').map((z, j) => {
+                  return (
+                    <a key={j} href="/" className="s-tag mr4">
+                      {z}
+                    </a>
+                  );
+                })}
+              </p>
+            </div>
+            <div className="mt24">
+              <div className="fs-headline1">{Answer.length} Answer</div>
+              {Answer.map((x, i) => {
                 return (
-                  <a key={j} href="/" className="s-tag mr4">
-                    {z}
-                  </a>
+                  <div key={i} className="py12 pr24 bb bc-black-075">
+                    <div
+                      dangerouslySetInnerHTML={{ __html: x.answer_body }}
+                      className="ta-left"
+                    ></div>
+                    <div className="ta-right">
+                      <span>{x.answer_author}</span>
+                      <span className="fc-black-500 mx16">{x.user_id}</span>
+                      <span className="fc-black-500">{x.created_at}</span>
+                    </div>
+                  </div>
                 );
               })}
-            </p>
-          </div>
-          <div className="mt24">
-            <div className="fs-headline1">{Answer.length} Answer</div>
-            {Answer.map((x, i) => {
-              return (
-                <div key={i} className="py12 pr24 bb bc-black-075">
-                  <div
-                    dangerouslySetInnerHTML={{ __html: x.answer_body }}
-                    className="ta-left"
-                  ></div>
-                  <div className="ta-right">
-                    <span>{x.answer_author}</span>
-                    <span className="fc-black-500 mx16">{x.user_id}</span>
-                    <span className="fc-black-500">{x.created_at}</span>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <EditoreWrapper>
-            <div className="my24 fs-headline1">Your Answer</div>
-            <Editor
-              initialValue="INSERT ANSWER"
-              previewStyle="vertical" // 미리보기 스타일 지정
-              height="300px" // 에디터 창 높이
-              initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
-              hideModeSwitch="true"
-              useCommandShortcut={true}
-            />
-            <div className="ta-right my12">
-              <button className="s-btn s-btn__primary" onClick={handleClick}>
-                Post Your Answer
-              </button>
             </div>
-          </EditoreWrapper>
-        </PostArea>
-        <NavSide2 />
-      </MainBox>
+            <EditoreWrapper>
+              <div className="my24 fs-headline1">Your Answer</div>
+              <Editor
+                initialValue="INSERT ANSWER"
+                previewStyle="vertical" // 미리보기 스타일 지정
+                height="300px" // 에디터 창 높이
+                initialEditType="wysiwyg" // 초기 입력모드 설정(디폴트 markdown)
+                hideModeSwitch="true"
+                useCommandShortcut={true}
+              />
+              <div className="ta-right my12">
+                <button className="s-btn s-btn__primary" onClick={handleClick}>
+                  Post Your Answer
+                </button>
+              </div>
+            </EditoreWrapper>
+          </PostArea>
+          <NavSide2 />
+        </MainBox>
+      </div>
     </Wrapper>
   );
 };
