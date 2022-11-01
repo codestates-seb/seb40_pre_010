a14234cd6c4b412cb95f11777a89d9c2fda19ecd
jaehongg <tkwh1133@gamil.com>
2022-11-01 19:28:30 +0900

    ⛲ feat : setupProxy 추가

---
+++ front/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    ['/user', '/question', '/answer'],
+    createProxyMiddleware({
+      target: 'https://1814-211-179-90-114.jp.ngrok.io/',
+      changeOrigin: true,
+    })
+  );
+};
