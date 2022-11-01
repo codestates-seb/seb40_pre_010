b3627ad96a279ca531108eb07e93b826048f5d59
jaehongg <tkwh1133@gamil.com>
2022-11-01 19:30:14 +0900

    ⛲ feat : body값 state 설정
    주석제거, title state처리해야함, tags구현중

--- front/src/components/TextEditor.js
+++ front/src/components/TextEditor.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { forwardRef } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import styled from 'styled-components';
@@ -12,26 +12,21 @@ const EditorWrapper = styled.div`
     flex-wrap: wrap;
   }
 `;
-const TextEditor = () => {
-  const [text, setText] = useState('');
-  const editorRef = React.createRef();
-  const onChange = () => {
-    const data = editorRef.current.getInstance().getHTML(); // getHTML or getMarkdown
-    setText(data);
-    console.log(text);
-  };
+const TextEditor = forwardRef((props, ref) => {
   return (
     <EditorWrapper>
       <Editor
         initialValue=" " // 초기 입력값
         previewStyle="tab" // vertical로 설정시 미리보기 화면 분할가능
         initialEditType="markdown" // markdown or wysiwyg
-        ref={editorRef}
-        onChange={onChange}
         height="500px"
+        onChange={props.onChange}
+        ref={ref}
         // width="100%"
       />
     </EditorWrapper>
   );
-};
+});
+TextEditor.displayName = 'TextEditor';
+
 export default TextEditor;
