a5b9317f2255525c985219e88b73da9d49acaee6
jaehongg <tkwh1133@gamil.com>
2022-11-04 14:36:29 +0900

    🖼 style : delete 위치 수정

--- front/src/pages/QuestionPost.js
+++ front/src/pages/QuestionPost.js
@@ -28,10 +28,8 @@ const PostArea = styled.div`
     margin-bottom: 8px;
     margin-right: 16px;
   }
-  & delete {
-    border: none;
-    background-color: white;
-    border-radius: 3px;
+  & .BTN {
+    margin-right: 10px;
   }
 `;
 const EditoreWrapper = styled.div`
@@ -80,10 +78,10 @@ const QuestionPost = () => {
 
   //setIsLogin(localStorage.getItem('token') ? true : false);
   console.log(IsLogin);
-  // useEffect(() => {
-  //   setTags(posts.questionTags);
-  //   setAnswers(posts.answers);
-  // }, [posts]);
+  useEffect(() => {
+    setTags(posts.questionTags);
+    setAnswers(posts.answers);
+  }, [posts]);
 
   const onchangehandle = () => {
     setAnswer(editorRef.current?.getInstance().getMarkdown());
@@ -142,7 +140,7 @@ const QuestionPost = () => {
                 <Viewer initialValue={posts.questionBody} />
               ) : null}
 
-              <div className="d-flex jc-space-between mb8">
+              <div className="d-flex jc-space-between mb8 fd-column">
                 <p>
                   {tags !== undefined
                     ? tags.split(',').map((z, j) => {
@@ -154,19 +152,33 @@ const QuestionPost = () => {
                       })
                     : null}
                 </p>
-                <div>
-                  {localStorage.getItem('userId') === userId ? (
-                    <button onClick={onClickDelete} className="delete">
-                      delete
+                <div className="d-flex jc-space-between">
+                  <div>
+                    <button className="s-link s-link__muted BTN">Share</button>
+                    {localStorage.getItem('userId') === userId ? (
+                      <div>
+                        <button className="s-link s-link__muted BTN">
+                          Edit
+                        </button>
+                        <button
+                          onClick={onClickDelete}
+                          className="s-link s-link__muted BTN"
+                        >
+                          delete
+                        </button>
+                      </div>
+                    ) : null}
+                    <button className="s-link s-link__muted BTN">
+                      Following
                     </button>
-                  ) : null}
-                </div>
+                  </div>
 
-                <UserCard
-                  pic={pic}
-                  author={posts.userId}
-                  variation={posts.questionId}
-                />
+                  <UserCard
+                    pic={pic}
+                    author={posts.userId}
+                    variation={posts.questionId}
+                  />
+                </div>
               </div>
             </div>
             <div className="mt24">
