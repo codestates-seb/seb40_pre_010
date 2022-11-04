5f6b52049195ca74622ce59699b81253b26bf2e1
choizia0724 <drama_days@naver.com>
2022-11-04 16:13:31 +0900

    🔨 refactor : 링크 변경

--- front/src/components/User-card.js
+++ front/src/components/User-card.js
@@ -3,11 +3,11 @@ const UserCard = (props) => {
   return (
     <div className="as-end s-user-card s-user-card__highlighted">
       <time className="s-user-card--time">{props.variation} min ago</time>
-      <a href="..." className="s-avatar s-avatar__32 s-user-card--avatar">
+      <a href="/" className="s-avatar s-avatar__32 s-user-card--avatar">
         <img className="s-avatar--image" src={props.pic} alt="profile" />
       </a>
       <div className="s-user-card--info">
-        <a href="…" className="s-user-card--link">
+        <a href="/" className="s-user-card--link">
           {props.author}
         </a>
         <ul className="s-user-card--awards">
